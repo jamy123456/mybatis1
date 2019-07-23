eb9202f71a9da4044a6d541e22f06a1a17a2e8bd
jamy123456 <990134943@qq.com>
2019-07-23 17:29:07 +0800

    修改映射文件

--- MybatisTest1/simple2/src/main/java/tk/mybatis/simple/mapper/UserMapperTest.java
+++ MybatisTest1/simple2/src/main/java/tk/mybatis/simple/mapper/UserMapperTest.java
@@ -10,27 +10,41 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import tk.mybatis.simple.model.User;
 
 
 public class UserMapperTest {
+	private static Logger logger = LoggerFactory.getLogger(UserMapperTest.class);
 	private static SqlSessionFactory sqlSessionFactory;
 	
 	@BeforeClass	
 	public static void init() {
 		try {
+			//初始化读取mybatis-config.xml文件
 			Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
 			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
 		} catch (IOException e) {
-			e.printStackTrace();
+			logger.info("==========异常信息：{}===========",e.getMessage());
 		}
 	}
 	
 	@Test
 	public void selectAll() {
+		//打开sql会话
 		SqlSession sqlSession = sqlSessionFactory.openSession();
-		List<User> list = sqlSession.selectList("query");
+		try {
+			List<User> list = sqlSession.selectList("query");
+			logger.info("=========查询结果：{}=========",list.toString());
+		}finally {
+			sqlSession.close();
+		}
+		
+		
+		
+		
 	}
 
 }
