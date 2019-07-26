6f1a07ed5e99d39a10b5f60bc0738ff468666efd
jamy123456 <990134943@qq.com>
2019-07-26 16:58:42 +0800

    userMapper.xml增加增删改方法

--- MybatisTest1/simple2/src/main/java/tk/mybatis/simple/mapper/TestUserAndRole.java
+++ MybatisTest1/simple2/src/main/java/tk/mybatis/simple/mapper/TestUserAndRole.java
@@ -33,19 +33,76 @@ public class TestUserAndRole {
 	}
 	
 	
+//	@Test
+//	public void testUserRole() {
+//		SqlSession sqlSession = null;
+//		try {
+//			sqlSession = getSession();
+//			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//			User user = userMapper.nestQuery(1);
+//			logger.info("=======User:{}========", user.toString());
+//			
+//			Assert.assertNotNull(user);
+//			
+//			Assert.assertNotNull(user.getRole());
+//		} finally {
+//			if (null != sqlSession) {
+//				sqlSession.close();
+//			}
+//		}
+//	}
+	
+//	@Test
+//	public void testInsertUser() {
+//		SqlSession sqlSession = null;
+//		try {
+//			sqlSession = getSession();
+//			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//			User user = new User();
+//			user.setName("李四");
+//			user.setAge("23");
+//			user.setAddress("安徽池州");
+//			int a = userMapper.insertUser(user);
+//			logger.info("=======插入{}个用户,用户信息为:{}========", a, user.toString());			
+//		} finally {
+//			sqlSession.commit();
+//			if (null != sqlSession) {
+//				sqlSession.close();
+//			}
+//		}
+//	}
+	
+//	@Test
+//	public void testUpdateUser() {
+//		SqlSession sqlSession = null;
+//		try {
+//			sqlSession = getSession();
+//			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//			User user = new User();
+//			user.setName("张三");
+//			user.setId(10);
+//			int a = userMapper.updateUser(user);
+//			logger.info("=======修改{}个用户,用户信息为:{}========", a, user.toString());			
+//		} finally {
+//			sqlSession.commit();
+//			if (null != sqlSession) {
+//				sqlSession.close();
+//			}
+//		}
+//	}
+	
 	@Test
-	public void testUserRole() {
+	public void testDeleteUser() {
 		SqlSession sqlSession = null;
 		try {
 			sqlSession = getSession();
 			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-			User user = userMapper.nestQuery(1);
-			logger.info("=======User:{}========", user.toString());
-			
-			Assert.assertNotNull(user);
-			
-			Assert.assertNotNull(user.getRole());
+			User user = new User();
+			user.setId(8);
+			int a = userMapper.deleteUser(user);
+			logger.info("=======删除{}个用户,用户信息为:{}========", a, user.toString());			
 		} finally {
+			sqlSession.commit();
 			if (null != sqlSession) {
 				sqlSession.close();
 			}
--- MybatisTest1/simple2/src/main/java/tk/mybatis/simple/mapper/UserMapper.java
+++ MybatisTest1/simple2/src/main/java/tk/mybatis/simple/mapper/UserMapper.java
@@ -5,4 +5,10 @@ import tk.mybatis.simple.model.User;
 public interface UserMapper {
 	
 	User nestQuery(int id);
+	
+	int insertUser(User user);
+	
+	int updateUser(User user);
+	
+	int deleteUser(User user);
 }
